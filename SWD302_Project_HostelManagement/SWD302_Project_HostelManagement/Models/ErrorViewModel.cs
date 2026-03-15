842f5c5922547b93d1bfe9aec694fa2d8c49ecfb
Phạm Gia Khánh <phamgiakhanh0709@gmail.com>
2026-03-15 14:02:37 +0700

    Add initial ASP.NET Core MVC project
    Add initial scaffold for SWD302_Project_HostelManagement: solution and project files, Program.cs, HomeController, ErrorViewModel, views (_Layout, Index, Privacy, Error), view imports/start, appsettings and launchSettings, site CSS/JS and static libs (Bootstrap, jQuery, validation)

---
+++ SWD302_Project_HostelManagement/SWD302_Project_HostelManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SWD302_Project_HostelManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
