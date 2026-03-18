4099ed2b3c957be533839435e423b928ed8bdc18
Lam Kieu <lamkdhe180931@fpt.edu.vn>
2026-03-18 19:49:59 +0700

    add usecase 3+4

--- SWD302_Project_HostelManagement/SWD302_Project_HostelManagement/Migrations/20260318115114_AddPriceToHostel.cs
+++ SWD302_Project_HostelManagement/SWD302_Project_HostelManagement/Migrations/20260318115114_AddPriceToHostel.cs
@@ -18,6 +18,17 @@ namespace SWD302_Project_HostelManagement.Migrations
                 scale: 2,
                 nullable: false,
                 defaultValue: 0m);
+
+            migrationBuilder.Sql("""
+                UPDATE h
+                SET h.price = room_prices.min_price
+                FROM Hostel AS h
+                INNER JOIN (
+                    SELECT hostel_id, MIN(price_per_month) AS min_price
+                    FROM Room
+                    GROUP BY hostel_id
+                ) AS room_prices ON h.hostel_id = room_prices.hostel_id
+                """);
         }
 
         /// <inheritdoc />
